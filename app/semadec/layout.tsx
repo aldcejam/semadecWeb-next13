f8ab0a5c91fb22386611daf57cb6c9de6edaaf19
aldcejam <aldcejamj923@gmail.com>
2022-12-06 19:22:37 -0300

    dark mode button

--- app/semadec/layout.tsx
+++ app/semadec/layout.tsx
@@ -2,9 +2,7 @@
 
 import { ContainerGlobal, ContainerPage } from '../globals';
 
-/* import SidebarPage from '../../src/components/templates/SidebarPage/Index'; */
 import SettingsButtons from '../../src/components/Molecules/SettingsButtons/Index';
-import { StyledSidebar } from '../../src/components/templates/Sidebar/Styled';
 import Sidebar from '../../src/components/templates/Sidebar/Index';
 
 export default function RootLayout({
@@ -17,7 +15,6 @@ export default function RootLayout({
         <>
             {/*  */}
             <ContainerGlobal>
-                {/* <SidebarPage /> */}
                 <Sidebar/>
                 <SettingsButtons />
                 {/* Same as */}

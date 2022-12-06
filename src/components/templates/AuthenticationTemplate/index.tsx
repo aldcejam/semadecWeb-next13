8c7bc92f9d237aaf89eadf30ea2593a690dc39af
aldcejam <aldcejamj923@gmail.com>
2022-12-06 14:12:48 -0300

    fixing imports

--- src/components/templates/AuthenticationTemplate/index.tsx
+++ src/components/templates/AuthenticationTemplate/index.tsx
@@ -1,5 +1,5 @@
-import OptionsLogin from "../../Molecules/AuthenticationTemplate/OptionsLogin";
-import Titles from "../../Molecules/AuthenticationTemplate/Titles";
+import OptionsLogin from "../../Molecules/AuthenticationTemplate/OptionsLogin/Index";
+import Titles from "../../Molecules/AuthenticationTemplate/Titles/Index";
 import Form from "../../Organisms/AuthenticationTemplate/Form/Index";
 import { StyledAuthenticationTemplate } from "./Styled";
 

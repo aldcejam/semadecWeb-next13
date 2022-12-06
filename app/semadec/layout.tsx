0ad21b1629cefa1ad40572ce9cae435b0210f662
aldcejam <aldcejamj923@gmail.com>
2022-12-06 18:32:13 -0300

    side bar

--- app/semadec/layout.tsx
+++ app/semadec/layout.tsx
@@ -4,8 +4,8 @@ import { ContainerGlobal, ContainerPage } from '../globals';
 
 /* import SidebarPage from '../../src/components/templates/SidebarPage/Index'; */
 import SettingsButtons from '../../src/components/Molecules/SettingsButtons/Index';
-
-
+import { StyledSidebar } from '../../src/components/templates/Sidebar/Styled';
+import Sidebar from '../../src/components/templates/Sidebar/Index';
 
 export default function RootLayout({
     children,
@@ -18,6 +18,7 @@ export default function RootLayout({
             {/*  */}
             <ContainerGlobal>
                 {/* <SidebarPage /> */}
+                <Sidebar/>
                 <SettingsButtons />
                 {/* Same as */}
             </ContainerGlobal>
---
+++ src/components/templates/Sidebar/Index.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { StyledSidebar } from "./Styled";
+
+
+
+const Sidebar = () => {
+
+    return (
+        <StyledSidebar menuisactive={"true"}>
+            
+        </StyledSidebar>
+    )
+}
+
+export default Sidebar;

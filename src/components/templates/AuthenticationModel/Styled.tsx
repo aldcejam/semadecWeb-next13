276cfc14f60dc9fd05baaaf8324f01703acceb5c
aldcejam <aldcejamj923@gmail.com>
2022-12-07 21:02:49 -0300

    login form in center

--- src/components/templates/AuthenticationModel/Styled.tsx
+++ src/components/templates/AuthenticationModel/Styled.tsx
@@ -2,7 +2,13 @@ import { styled } from "@mui/material";
 
 export const StyledAuthenticationTemplate = styled("div")`
     flex-basis: 55%;
-
-    .content{}
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .content{
+        position: absolute;
+        width: 100%;
+        max-width: 400px;
+    }
     
 `
